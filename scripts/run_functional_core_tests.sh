96c35265fe1fadcf1c19eb70730a8c9975b3f6c9
Xuliang <88468868+DingdongD@users.noreply.github.com>
2026-08-04 15:20:51 +0800

    Run credit-safe bucket decision pipeline regression

--- scripts/run_functional_core_tests.sh
+++ scripts/run_functional_core_tests.sh
@@ -10,16 +10,18 @@ run_tb() {
     local top="$1"
     local tb="$2"
     local out="${BUILD}/${top}.vvp"
+    local log="${BUILD}/${top}.log"
     echo "[compile] ${top}"
     iverilog -g2012 -Wall -s "${top}" -o "${out}" "${tb}" "${RTL_FILES[@]}"
     echo "[run] ${top}"
-    (cd "${ROOT}" && vvp "${out}")
+    (cd "${ROOT}" && vvp "${out}") | tee "${log}"
 }
 
 run_tb tb_max_tree4_tie "${ROOT}/tb/tb_max_tree4_tie.v"
 run_tb tb_point_engine_top_tail "${ROOT}/tb/tb_point_engine_top_tail.v"
 run_tb tb_point_engine_top_cycle "${ROOT}/tb/tb_point_engine_top_cycle.v"
 run_tb tb_quickfps_core_end2end "${ROOT}/tb/tb_quickfps_core_end2end.v"
+run_tb tb_bucket_decision_pipe "${ROOT}/tb/tb_bucket_decision_pipe.v"
 run_tb tb_pingpong_chunk_ctrl "${ROOT}/tb/tb_pingpong_chunk_ctrl.v"
 run_tb tb_pingpong_chunk_activity "${ROOT}/tb/tb_pingpong_chunk_activity.v"
 run_tb tb_sram_1r1w_activity "${ROOT}/tb/tb_sram_1r1w_activity.v"
