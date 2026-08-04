f66d0c7674729410215541c88de6b67306d39233
Xuliang <88468868+DingdongD@users.noreply.github.com>
2026-08-04 15:49:19 +0800

    Export credit-safe cycle scheduler as the public model

--- cmodel/quickfps_cycle/__init__.py
+++ cmodel/quickfps_cycle/__init__.py
@@ -1,7 +1,8 @@
 """Cycle-accurate timing model for the QuickFPS accelerator."""
 
 from .config import AcceleratorConfig, DramConfig
-from .model import QuickFPSCycleModel, SimulationResult
+from .cycle_model import QuickFPSCycleModel
+from .model import SimulationResult
 from .workload import BucketAction, BucketWork, IterationWork, Workload
 
 __all__ = [
